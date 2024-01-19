8bf152c0c32655dadc91767eb041f1dffa149400
dtishkina <lfhmztshkn@gmail.com>
2024-01-19 12:36:40 +0300

    add delete dialog for sale

---
+++ client/src/components/DeleteSaleDialog.vue
@@ -0,0 +1,76 @@
+<script>
+
+import {defineComponent} from "vue";
+import CustomBlock from "@/components/UI/CustomBlock.vue";
+import axios from "axios";
+
+export default defineComponent({
+  components: {CustomBlock},
+  props: {
+    show: {
+      type: Boolean,
+      required: true
+    },
+    itemName: {
+      type: [Number, String],
+      required: true
+    },
+    itemId: {
+      type: [Number, String],
+      required: true
+    },
+  },
+
+  methods: {
+    cancelDelete() {
+      this.$emit('closeDelete');
+    },
+    confirmDelete() {
+      console.log(this.itemId)
+      axios.delete(`rest/sales/${this.itemName}`)
+        .then(() => {
+          this.$emit('deleted');
+        })
+        .catch(error => {
+          console.error('Error deleting the item:', error);
+        });
+    },
+  }
+})
+</script>
+
+<template>
+  <v-dialog v-model="show" max-width="380px">
+    <v-card style="border-radius: 20px">
+      <custom-block>
+
+          <v-card-text class="text-h6" style="text-align: center;">
+            Вы действительно хотите удалить заявку
+            {{ itemName }} на товар {{ itemId }}?
+          </v-card-text>
+
+        <v-card-actions style="margin: 0 35px">
+          <v-container>
+            <v-btn
+              style="border: 2px solid lavender; border-radius: 14px; margin: 0 8px;"
+              variant="outlined"
+              @click="cancelDelete">
+              Отмена
+            </v-btn>
+            <v-btn
+              style="border: 2px solid lavender; border-radius: 14px; background-color:lavender; margin: 0 8px;"
+              variant="outlined"
+              @click="confirmDelete">
+              Удалить
+            </v-btn>
+          </v-container>
+        </v-card-actions>
+
+      </custom-block>
+    </v-card>
+  </v-dialog>
+</template>
+
+<style>
+
+</style>
