9eb71682be3700f76d5cf63e22f3b4329697f992
tishina <lfhmztshkn@gmail.com>
2024-06-03 17:22:00 +0300

    add error page files

---
+++ client/src/components/forms/ErrorForm.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+import CustomBlock from "@/components/UI/CustomBlock.vue";
+</script>
+
+<template>
+  <custom-block class="custom-block">
+    <div class="block">
+      <v-alert-title class="error-title">Ошибка 404</v-alert-title>
+      <div class="error-message">Страница не найдена, попытайтесь еще раз!</div>
+      <v-img class="img" src="C:\Users\tiski\IdeaProjects\automize-manage\client\src\assets\error.png"></v-img>
+    </div>
+  </custom-block>
+</template>
+
+<style scoped>
+
+.custom-block{
+  border: 2px solid black;
+  border-radius: 20px;
+  height: 20lh;
+}
+
+.block{
+  padding: 50px;
+}
+
+.error-title{
+  font-size: 60px;
+  margin-bottom: 30px;
+}
+
+.error-message{
+  font-size: 30px;
+  color: mediumpurple;
+}
+
+.img{
+  width: 150px;
+  height: 150px;
+  margin: 0 0 0 300px;
+}
+
+</style>
---
+++ client/src/layouts/pages/ErrorPage.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import ErrorForm from "@/components/forms/ErrorForm.vue";
+</script>
+
+<template>
+  <error-form class="error-form"></error-form>
+</template>
+
+<style scoped>
+.error-form{
+  width: 30lh;
+  margin: 100px auto 200px;
+}
+</style>
