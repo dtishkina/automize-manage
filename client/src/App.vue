2ea50f833e4acf80f4927f939841370911ce88a0
tishina <lfhmztshkn@gmail.com>
2024-06-07 20:22:56 +0300

    fix pages position

--- client/src/App.vue
+++ client/src/App.vue
@@ -17,7 +17,6 @@ import Navbar from '@/components/NavbarComp.vue'
 import NoInfoHeader from "@/components/UI/NoInfoHeader.vue";
 export default {
   name: 'App',
-
   components: {
     NoInfoHeader,
     Navbar,
@@ -33,12 +32,15 @@ export default {
 }
 
 .app-content {
+  display: flex;
+  justify-content: center;
+  width: 1144px;
   flex: 1;
   font-family: Manrope, ui-rounded;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: left;
   color: #2c3e50;
-  margin: 40px 60px;
+  margin: 40px 60px 120px;
 }
 </style>
