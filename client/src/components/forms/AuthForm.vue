1759bd3914f43463430e795177b464a5631326d7
tishina <lfhmztshkn@gmail.com>
2024-06-07 16:59:38 +0300

    add snackbar

--- client/src/components/forms/AuthForm.vue
+++ client/src/components/forms/AuthForm.vue
@@ -10,6 +10,8 @@ export default {
     return {
       username: '',
       password: '',
+      snackbar: false,
+      text: 'Неверные учетные данные',
     };
   },
   methods: {
@@ -18,15 +20,15 @@ export default {
       formData.append('username', this.username);
       formData.append('password', this.password);
 
-      axios.post('http://localhost:8080/login', formData)
+      axios.post('/login', formData)
         .then(() => {
           window.location.href = '/references';
         })
         .catch(error => {
           console.error('Error on login:', error);
-          window.location.href = '/error';
+          this.snackbar = true;
         });
-    }
+    },
   },
 }
 </script>
@@ -35,42 +37,56 @@ export default {
   <custom-block class="block">
     <v-form @submit.prevent="login">
 
-    <v-card-title class="headline-block">
-      <span class="headline">Добро пожаловать</span>
-    </v-card-title>
+      <v-card-title class="headline-block">
+        <span class="headline">Добро пожаловать</span>
+      </v-card-title>
 
-    <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-    <v-card-text>
-      <v-container>
-        <v-text-field
-          v-model="username"
-          label="Имя пользователя"
-          required
-          rounded
-          variant="outlined">
-        </v-text-field>
-        <v-text-field
-          v-model="password"
-          label="Пароль"
-          type="password"
-          required
-          rounded
-          variant="outlined">
-        </v-text-field>
-      </v-container>
-    </v-card-text>
+      <v-card-text>
+        <v-container>
+          <v-text-field
+            v-model="username"
+            label="Имя пользователя"
+            required
+            rounded
+            variant="outlined">
+          </v-text-field>
+          <v-text-field
+            v-model="password"
+            label="Пароль"
+            type="password"
+            required
+            rounded
+            variant="outlined">
+          </v-text-field>
+        </v-container>
+      </v-card-text>
 
-    <v-card-actions class="actions-comp">
-      <v-btn  type="submit" class="submit-btn">Войти</v-btn>
-    </v-card-actions>
+      <v-card-actions class="actions-comp">
+        <v-btn type="submit" class="submit-btn">Войти</v-btn>
+
+        <v-snackbar
+          v-model="snackbar"
+          rounded="pill"
+          color="red"
+          variant="tonal"
+          style="margin-bottom: 660px;"
+        >
+          {{ text }}
+          <template v-slot:actions>
+            <v-btn variant="text" @click="snackbar = false">
+              <img src="\src\assets\x-close.svg" style="height: 24px" alt="x">
+            </v-btn>
+          </template>
+        </v-snackbar>
+      </v-card-actions>
     </v-form>
   </custom-block>
 </template>
 
 <style scoped>
-
-.submit-btn{
+.submit-btn {
   background-color: black;
   color: white;
   width: 280px;
@@ -79,21 +95,20 @@ export default {
   border-radius: 20px;
 }
 
-.actions-comp{
+.actions-comp {
   margin-bottom: 16px;
 }
 
-.block{
+.block {
   border: 2px solid black;
-  border-radius: 20px
+  border-radius: 20px;
 }
 
-.headline{
+.headline {
   margin: 50px;
 }
 
-.headline-block{
+.headline-block {
   margin-top: 16px;
 }
-
 </style>
