69eddbc6657faf79381500d0a51c7b1798352faf
tishina <lfhmztshkn@gmail.com>
2024-06-10 21:26:40 +0300

    made EditGoodDialog as separate component

---
+++ client/src/components/dialogs/EditGoodDialog.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-dialog v-model="dialog" max-width="600px">
+    <v-card style="border-radius: 20px;">
+      <custom-block style="padding: 0 30px">
+        <v-card-title style="margin: 20px 0;">
+          <span class="text-h5">{{ formTitle }}</span>
+        </v-card-title>
+        <v-card-text style="padding: 0 10px">
+          <v-row>
+            <v-col>
+              <v-text-field
+                label="Наименование"
+                rounded
+                variant="outlined"
+                v-model="editedItem.name">
+              </v-text-field>
+            </v-col>
+            <v-col>
+              <v-text-field
+                v-model="editedItem.priority"
+                rounded
+                variant="outlined"
+                label="Приоритет">
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-text-field
+                label="Количество на складе #1"
+                rounded
+                variant="outlined"
+                v-model="editedItem.goodCountFirst">
+              </v-text-field>
+            </v-col>
+            <v-col>
+              <v-text-field
+                label="Количество на складе #2"
+                rounded
+                variant="outlined"
+                v-model="editedItem.goodCountSecond">
+              </v-text-field>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions style="padding: 0 0 20px 100px">
+          <v-container>
+            <v-spacer></v-spacer>
+            <v-btn
+              style="border: 2px solid lavender; border-radius: 14px; margin: 0 8px; width: 34%"
+              variant="outlined"
+              size="large"
+              @click="close">
+              Отмена
+            </v-btn>
+            <v-btn
+              style="border: 2px solid lavender; border-radius: 14px; background-color:lavender; margin: 0 8px;"
+              variant="outlined"
+              size="large"
+              @click="save">
+              Сохранить
+            </v-btn>
+          </v-container>
+        </v-card-actions>
+      </custom-block>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import CustomBlock from "@/components/UI/CustomBlock.vue";
+
+export default defineComponent({
+  name: "EditGoodDialog",
+  components: { CustomBlock },
+  props: {
+    dialog: {
+      type: Boolean,
+      required: true
+    },
+    editedIndex: {
+      type: Number,
+      required: true
+    },
+    editedItem: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? 'Новый товар' : 'Редактировать товар';
+    }
+  },
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+    save() {
+      this.$emit('save');
+    }
+  }
+});
+</script>
+
+<style scoped>
+</style>
--- client/src/components/tables/GoodsDataTable.vue
+++ client/src/components/tables/GoodsDataTable.vue
@@ -6,119 +6,39 @@
                   :items-length="totalItems"
                   :items="serverItems"
                   :search="search"
-                  @update:options="loadItems"
-    >
-
+                  @update:options="loadItems">
       <template v-slot:top>
         <custom-block class="search-header">
           <v-text-field class="search-bar"
                         v-model="search"
                         density="compact"
-                        rounded variant="outlined"
+                        rounded
+                        variant="outlined"
                         placeholder="Найти товар">
           </v-text-field>
-
           <v-spacer></v-spacer>
-
           <custom-button @click="generatePdf"
-          button-text="Скачать отчёт в PDF"
-          style="border: 2px solid lavender; border-radius: 14px;
-                padding-top: 8px; background-color:lavender"></custom-button>
-
+                         button-text="Скачать отчёт в PDF"
+                         style="border: 2px solid lavender; border-radius: 14px;
+                                padding-top: 8px; background-color:lavender">
+          </custom-button>
           <v-spacer></v-spacer>
-
-          <v-dialog v-model="dialog" max-width="600px">
-            <template v-slot:activator="{ props }">
-              <custom-button
-                v-bind="props"
-                style="border: 2px solid lavender; border-radius: 14px;
-                                 padding-top: 8px; background-color:lavender"
-                button-text="Новый товар"
-                :svg-path="'src/assets/plus.svg'">Новый товар</custom-button>
-            </template>
-
-            <v-card style="border-radius: 20px;">
-              <custom-block style="padding: 0 30px">
-              <v-card-title style="margin: 20px 0;">
-                <span class="text-h5">{{ formTitle }}</span>
-              </v-card-title>
-
-                <v-card-text style="padding: 0 10px">
-                  <v-row>
-                    <v-col>
-                      <v-text-field
-                        label="Наименование"
-                        rounded
-                        variant="outlined"
-                        v-model="editedItem.name">
-                      </v-text-field>
-                    </v-col>
-                    <v-col>
-                      <v-text-field
-                        v-model="editedItem.priority"
-                        rounded
-                        variant="outlined"
-                        label="Приоритет"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                  <v-row>
-                    <v-col>
-                      <v-text-field
-                        label="Количество на складе #1"
-                        rounded
-                        variant="outlined"
-                        v-model="editedItem.goodCountFirst">
-                      </v-text-field>
-                    </v-col>
-                    <v-col>
-                      <v-text-field
-                        label="Количество на складе #2"
-                        rounded
-                        variant="outlined"
-                        v-model="editedItem.goodCountSecond">
-                      </v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-card-text>
-
-                <v-card-actions style="padding: 0 0 20px 100px">
-                <v-container>
-                <v-spacer></v-spacer>
-                <v-btn
-                  style="border: 2px solid lavender;
-                   border-radius: 14px; margin: 0 8px;
-                    width: 34%"
-                  variant="outlined"
-                  size="large"
-                  @click="close">
-                  Отмена
-                </v-btn>
-                <v-btn
-                  style="border: 2px solid lavender; border-radius: 14px; background-color:lavender; margin: 0 8px;"
-                  variant="outlined"
-                  size="large"
-                  @click="save">
-                  Сохранить
-                </v-btn>
-                </v-container>
-              </v-card-actions>
-              </custom-block>
-            </v-card>
-          </v-dialog>
-
+          <custom-button @click="openDialog"
+                         style="border: 2px solid lavender; border-radius: 14px;
+                                padding-top: 8px; background-color:lavender"
+                         button-text="Новый товар"
+                         :svg-path="'src/assets/plus.svg'">Новый товар
+          </custom-button>
+          <delete-dialog
+            :show="dialogDelete"
+            :itemName="editedItem.name"
+            :itemId="editedItem.id"
+            @close="closeDelete"
+            @deleted="handleDeletion"
+            @click="closeDelete">
+          </delete-dialog>
         </custom-block>
-
-        <delete-dialog
-          :show="dialogDelete"
-          :itemName="editedItem.name"
-          :itemId="editedItem.id"
-          @close="closeDelete"
-          @deleted="handleDeletion"
-          @click="closeDelete"
-        ></delete-dialog>
       </template>
-
       <template v-slot:item.actions="{ item }">
         <v-btn-group>
           <v-btn variant="text"
@@ -140,28 +60,31 @@
         </v-btn-group>
       </template>
       <template v-slot:no-data>
-        <v-btn color="primary" @click="loadItems"> Reset</v-btn>
+        <v-btn color="primary" @click="loadItems">Reset</v-btn>
       </template>
     </v-data-table>
+    <edit-good-dialog
+      :dialog="dialog"
+      :editedIndex="editedIndex"
+      :editedItem="editedItem"
+      @close="close"
+      @save="save">
+    </edit-good-dialog>
   </custom-block>
 </template>
 
 <style>
-
 .element {
   padding: 10px;
 }
-
-.search-bar{
+.search-bar {
   width: 45%;
 }
-
-.search-header{
+.search-header {
   height: 75px;
   display: flex;
   flex-direction: row;
 }
-
 </style>
 
 <script>
@@ -169,6 +92,7 @@ import CustomBlock from "@/components/UI/CustomBlock.vue";
 import getAllGoods from "@/services/getAllGoods";
 import CustomButton from "@/components/UI/CustomButton.vue";
 import DeleteDialog from "@/components/dialogs/DeleteGoodDialog.vue";
+import EditGoodDialog from "@/components/dialogs/EditGoodDialog.vue"; // Импортируем новый компонент
 import axios from "axios";
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
@@ -179,6 +103,7 @@ export default {
     DeleteDialog,
     CustomButton,
     CustomBlock,
+    EditGoodDialog, // Добавляем новый компонент
   },
   data() {
     return {
@@ -206,7 +131,7 @@ export default {
         goodCountFirst: '',
         goodCountSecond: '',
       },
-      defaultItem:{
+      defaultItem: {
         id: '',
         name: '',
         priority: '',
@@ -220,15 +145,15 @@ export default {
   },
   computed: {
     formTitle() {
-      return this.editedIndex === -1 ? 'Добавить товар' : 'Редактировать товар'
+      return this.editedIndex === -1 ? 'Добавить товар' : 'Редактировать товар';
     },
   },
   watch: {
     dialog(val) {
-      val || this.close()
+      val || this.close();
     },
     dialogDelete(val) {
-      val || this.closeDelete()
+      val || this.closeDelete();
     },
   },
   methods: {
@@ -237,7 +162,7 @@ export default {
       getAllGoods.fetch({
         page: 1,
         itemsPerPage: 10,
-      }).then(({items, total}) => {
+      }).then(({ items, total }) => {
         this.serverItems = items;
         this.totalItems = total;
         this.loading = false;
@@ -252,33 +177,35 @@ export default {
       this.loadItems();
     },
     editItem(item) {
-      this.editedIndex = this.serverItems.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
+      this.editedIndex = this.serverItems.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
     },
-
     deleteItem(item) {
-      this.editedIndex = this.serverItems.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialogDelete = true
+      this.editedIndex = this.serverItems.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
     },
-
+    openDialog() {
+      this.editedIndex = -1;
+      this.editedItem = Object.assign({}, this.defaultItem);
+      this.dialog = true;
+    },
     close() {
-      this.dialog = false
+      this.dialog = false;
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
     },
     closeDelete() {
-      this.dialogDelete = false
+      this.dialogDelete = false;
       this.show = false;
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
     },
-
     save() {
       if (this.editedIndex > -1) {
         axios.patch('/rest/goods/' + this.editedItem.id, this.editedItem)
@@ -297,9 +224,8 @@ export default {
             console.error('Error adding new item:', error);
           });
       }
-      this.close()
+      this.close();
     },
-
     generatePdf() {
       const body = this.serverItems.map(item => [
         item.id,
@@ -308,23 +234,19 @@ export default {
         item.goodCountFirst,
         item.goodCountSecond,
       ]);
-
       const tableHeader = ['ID', 'Наименование', 'Приоритет', 'На складе #1', 'На складе #2'];
       body.unshift(tableHeader);
-
       const docDefinition = {
         content: [
           {
             table: {
-              body: body
-            }
+              body: body,
+            },
           },
         ],
       };
-
       pdfMake.createPdf(docDefinition).download('all_goods.pdf');
     },
   },
 };
-
 </script>
